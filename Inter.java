c453a7b1ceea02b70cd4ea1a750c98b79e132a67
ruteshchavan <ruteshrc007@gmail.com>
2024-11-29 18:19:25 +0530

    add

--- Inter.java
+++ Inter.java
@@ -1,16 +1,22 @@
 /*
 Another way to achieve abstraction in Java, is with interfaces.
 
-
 Syntax :
-interface <interface_name>{  
+interface interface_name{  
     // declare constant fields  
     // declare methods that abstract   
     // by default.  
 }  
 
 To access the interface methods, the interface must be "implemented" (inherited) 
-by another class with the implements keyword (instead of extends).
+by another class with the (implements) keyword (instead of extends).
+
+3 types of interface ;
+normal interface - interface that has more than 1 methods.
+functional interface - interface that has only 1 method. Also known as single abstract method.
+marker interface  - the interface that has no method or a blank interface. 
+
+
 */
 
 interface FirstInterface {
@@ -32,9 +38,9 @@ class DemoClass implements FirstInterface, SecondInterface {
 
 public  class Inter {
 	public static void main(String[] args) {
-	    DemoClass myObj = new DemoClass();
+	    DemoClass myObj ;
+	    myObj= new DemoClass();
 	    myObj.myMethod();
 	    myObj.myOtherMethod();
 	  }
-
 }
