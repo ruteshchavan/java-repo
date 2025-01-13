6c234c61c595b9869211c20d39ec64217d60ca71
ruteshchavan <ruteshrc007@gmail.com>
2025-01-13 10:32:37 +0530

    add

--- Addition.java
+++ Addition.java
@@ -13,10 +13,8 @@ System.out.println("The sum of a and b is= " + c);
 //user defined method  
 public static int add(int n1, int n2)   //n1 and n2 are formal parameters  
 {  
-int s;  
-s=n1+n2;  
-return s; //returning the sum  
-}  
+	return n1+n2;
+	}  
 }  
 
 
